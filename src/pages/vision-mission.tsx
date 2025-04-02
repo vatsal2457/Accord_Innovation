181abf960ef0fa0d6e6a1683a35e038498c047f0
vatsal-vishal <vatsalvishal003@gmail.com>
2025-04-02 18:18:42 +0530

    change

--- src/pages/vision-mission.tsx
+++ src/pages/vision-mission.tsx
@@ -23,7 +23,7 @@ const VisionMission: React.FC = () => {
             <div 
               className="absolute inset-0 bg-cover bg-center bg-no-repeat"
               style={{
-                backgroundImage: 'url("https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")',
+                backgroundImage: 'url("https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1050&q=80")',
               }}
             />
           </div>
