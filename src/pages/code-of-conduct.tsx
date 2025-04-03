43cfee4dcb17042782ed4e2655d0ea901b0c11e4
vatsal-vishal <vatsalvishal003@gmail.com>
2025-04-03 07:46:15 +0530

    leadership

--- src/pages/code-of-conduct.tsx
+++ src/pages/code-of-conduct.tsx
@@ -23,7 +23,7 @@ const CodeOfConduct: React.FC = () => {
             <div 
               className="absolute inset-0 bg-cover bg-center bg-no-repeat"
               style={{
-                backgroundImage: 'url("https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1050&q=80")',
+                backgroundImage: 'url("https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")',
               }}
             />
           </div>
